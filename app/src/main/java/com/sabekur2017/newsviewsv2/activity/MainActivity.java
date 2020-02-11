1acfc014aabbc32a7adf154b3581aa9a1e0f75c2
sabekur <sabekur2020@gmail.com>
2020-02-11 09:53:40 +0600

    welcome screen done

--- app/src/main/java/com/sabekur2017/newsviewsv2/activity/MainActivity.java
+++ app/src/main/java/com/sabekur2017/newsviewsv2/activity/MainActivity.java
@@ -1,14 +1,22 @@
-package com.sabekur2017.newsviewsv2;
+package com.sabekur2017.newsviewsv2.activity;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.sabekur2017.newsviewsv2.R;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+
 }
